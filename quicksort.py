1ac5bc232df93f44a9bc9023fc5159dbfe3a6cfa
Isaac Mast <isaac.k.mast@gmail.com>
2015-10-20 02:46:02 -0400

    Modified code for array of strings

--- quicksort.py
+++ quicksort.py
@@ -1,12 +1,12 @@
-import random, timeit
+import random, string, timeit
 
 '''
-Determines the execution time of a quicksort algorithm given an array 
-of random integers of length arrayLength. I do not own this code. All 
-rights and original code of the quicksort algorithm belong to the 
-OpenDSA hypertextbook project, which is part of algoviz.org. All rights 
-and original code of the wrapper and wrapped functions belong to 
-Xiaonuo Gantan from Python Central. 
+Determines the execution time of a quicksort algorithm given an array, 
+length arrayLength, of random strings, length stringLength. I do not 
+own this code. All rights and original code of the quicksort algorithm 
+belong to the OpenDSA hypertextbook project, which is part of 
+algoviz.org. All rights and original code of the wrapper and wrapped 
+functions belong to Xiaonuo Gantan from Python Central. 
 '''
 
 def wrapper(func, *args, **kwargs):
@@ -20,10 +20,10 @@ def wrapper(func, *args, **kwargs):
 
 def quicksort(array, i, j):
     '''
-    Quicksort algorithm for sorting an array of integers
-    Input: An array of integers, array, left bound, i, and right 
+    Quicksort algorithm for sorting an array of random strings
+    Input: An array of strings, array, left bound, i, and right 
            bound, j
-    Output: The sorted array of integers
+    Output: The sorted array of strings
     '''
 
     #Determine pivot index
@@ -49,13 +49,11 @@ def quicksort(array, i, j):
     #Sort right subarray
     if j - k > 1:
         quicksort(array, k + 1, j)
-
-    return array
     
 def findpivot(array, i, j):
     '''
     Determines pivot element in array in a quicksort algorithm
-    Input: An array of integers, array, left bound, i, and right 
+    Input: An array of strings, array, left bound, i, and right 
            bound, j
     Output: The index of the pivot element
     '''
@@ -63,8 +61,8 @@ def findpivot(array, i, j):
 
 def partition(array, left, right, pivot):
     '''
-    Partitions an array of integers in a quicksort algorithm
-    Input: An array of integers, array, left bound, left, right 
+    Partitions an array of strings in a quicksort algorithm
+    Input: An array of strings, array, left bound, left, right 
            bound, right, and pivot index, pivot 
     Output: The index of left bound
     '''
@@ -79,7 +77,10 @@ def partition(array, left, right, pivot):
             array[right] = temp
     return left
 
-arrayLength = 50
-array = random.sample(range(1, 100), arrayLength)
+array = []
+arrayLength = 10
+stringLength = 10
+for i in range(arrayLength):
+	array.append(''.join(random.choice(string.lowercase) for i in range(stringLength)))
 wrapped = wrapper(quicksort, array, 0, arrayLength - 1)
 print timeit.timeit(wrapped, number = 1000)
